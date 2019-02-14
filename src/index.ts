59819b72ed130d4c0cff36864b4e8b9bd188c270
singuerinc <nahuel.scotti@gmail.com>
2019-02-14 23:29:21 +0100

    feat: typescriptify

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as cors from "cors";
 import * as express from "express";
-import graphqlHTTP from "express-graphql";
+import * as graphqlHTTP from "express-graphql";
 import cfg from "./config";
 import schema from "./schema";
 import { buildCredentials } from "./utils";
